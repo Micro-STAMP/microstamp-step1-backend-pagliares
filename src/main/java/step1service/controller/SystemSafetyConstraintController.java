f74a40ccf61dea80172db121fad411bb0b77d918
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-24 22:26:13 -0300

    Fix name of some variables not following Java guidelines

--- src/main/java/step1service/controller/SystemSafetyConstraintController.java
+++ src/main/java/step1service/controller/SystemSafetyConstraintController.java
@@ -8,45 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/SystemSafetyConstraints")
+@RequestMapping("api/system-safety-constraints")
 @AllArgsConstructor
 public class SystemSafetyConstraintController {
 
-    private SystemSafetyConstraintService SystemSafetyConstraintService;
+    private SystemSafetyConstraintService systemSafetyConstraintService;
 
-    // Build save SystemSafetyConstraint REST API
+    // Save SystemSafetyConstraint REST API
     @PostMapping
     public ResponseEntity<SystemSafetyConstraintDto> saveSystemSafetyConstraint(@RequestBody SystemSafetyConstraintDto systemSafetyConstraintDto){
-        SystemSafetyConstraintDto savedSystemSafetyConstraintDto = SystemSafetyConstraintService.saveSystemSafetyConstraint(systemSafetyConstraintDto);
+        SystemSafetyConstraintDto savedSystemSafetyConstraintDto = systemSafetyConstraintService.saveSystemSafetyConstraint(systemSafetyConstraintDto);
         return new ResponseEntity<SystemSafetyConstraintDto>(savedSystemSafetyConstraintDto, HttpStatus.CREATED);
     }
 
-    // Build get SystemSafetyConstraint REST API
-    @GetMapping("{SystemSafetyConstraint-code}")
-    public ResponseEntity<SystemSafetyConstraintDto> getSystemSafetyConstraint(@PathVariable("SystemSafetyConstraint-code") String SystemSafetyConstraintCode) {
-        SystemSafetyConstraintDto systemSafetyConstraintDto = SystemSafetyConstraintService.getSystemSafetyConstraintByCode(SystemSafetyConstraintCode);
+    // Get SystemSafetyConstraint REST API
+    @GetMapping("{system-safety-constraint-code}")
+    public ResponseEntity<SystemSafetyConstraintDto> getSystemSafetyConstraint(@PathVariable("system-safety-constraint-code") String systemSafetyConstraintCode) {
+        System.out.println(systemSafetyConstraintCode);
+        SystemSafetyConstraintDto systemSafetyConstraintDto = systemSafetyConstraintService.getSystemSafetyConstraintByCode(systemSafetyConstraintCode);
         return new ResponseEntity<SystemSafetyConstraintDto>(systemSafetyConstraintDto, HttpStatus.OK);
     }
-
 }
-    /*
-         Moved to a central class - GlobalExceptionHandler
-         @@ExceptionHandler is an annotation used to handle the SPECIFIC exceptions and
-         sending the custom responses to the client
-
-         @ExceptionHandler(ResourceNotFoundException.class)
-         public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
-                ResourceNotFoundException resourceNotFoundException,
-                WebRequest webRequest){
-            ErrorDetails errorDetails = new ErrorDetails(
-                    LocalDateTime.now(),
-                    resourceNotFoundException.getMessage(),
-                    webRequest.getDescription(false),
-                    "SystemSafetyConstraint_NOT_FOUND"
-            );
-
-            return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-        }
-     */
 
 
