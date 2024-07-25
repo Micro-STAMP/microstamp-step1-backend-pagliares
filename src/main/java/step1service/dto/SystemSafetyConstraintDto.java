a7af9233895823819220877ec423bca55a2e9412
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-24 22:27:55 -0300

    Fix name of some variables not following Java guidelines

--- src/main/java/step1service/dto/SystemSafetyConstraintDto.java
+++ src/main/java/step1service/dto/SystemSafetyConstraintDto.java
@@ -11,8 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SystemSafetyConstraintDto {
     private Long id;
-    private String SystemSafetyConstraintName;
-    private String SystemSafetyConstraintDescription;
-    private String SystemSafetyConstraintCode;
+    private String systemSafetyConstraintCode;
+    private String systemSafetyConstraintDescription;
 
 }
