84b5151cbc2b72d6e5dcb1b8d6d185007a1cf6dd
JoaoHugo <joaohmarinho99@gmail.com>
2024-07-28 11:52:49 -0300

    feat: authorization server integration example

--- src/main/java/step1service/controller/SystemSafetyConstraintController.java
+++ src/main/java/step1service/controller/SystemSafetyConstraintController.java
@@ -1,6 +1,8 @@
 package step1service.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import step1service.dto.SystemSafetyConstraintDto;
 import step1service.service.SystemSafetyConstraintService;
 import org.springframework.http.HttpStatus;
@@ -23,8 +25,10 @@ public class SystemSafetyConstraintController {
 
     // Get SystemSafetyConstraint REST API
     @GetMapping("{system-safety-constraint-code}")
-    public ResponseEntity<SystemSafetyConstraintDto> getSystemSafetyConstraint(@PathVariable("system-safety-constraint-code") String systemSafetyConstraintCode) {
-        System.out.println(systemSafetyConstraintCode);
+    public ResponseEntity<SystemSafetyConstraintDto> getSystemSafetyConstraint(@AuthenticationPrincipal Jwt jwt,
+                                                                               @PathVariable("system-safety-constraint-code")
+                                                                               String systemSafetyConstraintCode) {
+        System.out.println(jwt.getSubject());
         SystemSafetyConstraintDto systemSafetyConstraintDto = systemSafetyConstraintService.getSystemSafetyConstraintByCode(systemSafetyConstraintCode);
         return new ResponseEntity<SystemSafetyConstraintDto>(systemSafetyConstraintDto, HttpStatus.OK);
     }
